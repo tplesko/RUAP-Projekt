5a99c59c743ca3309ca5a877a92423db82c2a5a9
tea.plesko <tea.plesko15@gmail.com>
2024-11-06 10:07:21 +0100

    Dodan pozdrav s imenom i prezimenom u konzolnoj aplikaciji

---
+++ lv1_ruap/lv1_ruap/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lv1_ruap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Tee Pleško");
+        }
+    }
+}
